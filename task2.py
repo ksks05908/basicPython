054ee3d5947f5c364fd4935dca7cee8a3d923098
ksks05908 <70337224+ksks05908@users.noreply.github.com>
2020-09-06 16:50:35 +0500

    Add files via upload

--- task2.py
+++ task2.py
@@ -1,20 +1,14 @@
-# Для списка реализовать обмен значений соседних элементов, т.е.
-# Значениями обмениваются элементы с индексами 0 и 1, 2 и 3 и т.д.
-# При нечетном количестве элементов последний сохранить на своем месте.
-# Для заполнения списка элементов необходимо использовать функцию input().
-user_list = []
-while True:
-    new_el =  input('Введите элемент списка. Для окончания ввода введите out: ')
-    if new_el == 'out':
-        break
-    user_list.append(new_el)
-i = 0
-print(f'Список до обмена: {user_list}')
-try:
-    for el in user_list[0::2]:
-        user_list[i], user_list[i+1] = user_list[i+1], user_list[i]
-        i += 2
-except IndexError:
-    print('В списке нечетное кол-во элементов, последний элемент останется на прежнем месте')
-print(user_list)
+# Реализовать функцию, принимающую несколько параметров, описывающих данные пользователя:
+# имя, фамилия, год рождения, город проживания, email, телефон.
+# Функция должна принимать параметры как именованные аргументы.
+# Реализовать вывод данных о пользователе одной строкой.
+def output_user_info(name, surname, birthday, city, telephone, email):
+    print(f'Имя: {name},фамилия: {surname}, дата рождения: {birthday}, город {city}, почта {email}, телефон {telephone}')
 
+user_name = input('Введите имя: ')
+surname_user = input('Введите фамилию: ')
+birthday_user = input('Введите дату рождения: ')
+city_user = input('Введите город проживания: ')
+email_user = input('Введите почту: ')
+telephone_user = input('Введите телефон: ')
+output_user_info(name=user_name, surname=surname_user, birthday=birthday_user, city=city_user, email=email_user, telephone=telephone_user)
