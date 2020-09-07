f8a96a79eecc2a53f425818b79d483691dfe7af0
ksks05908 <70337224+ksks05908@users.noreply.github.com>
2020-09-07 22:21:21 +0500

    Add files via upload

--- task5.py
+++ task5.py
@@ -1,19 +1,12 @@
-# Реализовать структуру «Рейтинг», представляющую собой не возрастающий набор натуральных чисел.
-# У пользователя необходимо запрашивать новый элемент рейтинга.
-# Если в рейтинге существуют элементы с одинаковыми значениями,
-# то новый элемент с тем же значением должен разместиться после них.
-rating = [7, 5, 3, 3, 2]
-print(f'Рейтинг: {rating}')
-new_el = int(input('Введите новый элемент рейтинга: '))
-for i in range(len(rating)):
-    if new_el < rating[-1]:
-        rating.append(new_el)
-        break
-    elif new_el > rating[0]:
-        rating.insert(1, new_el)
-        break
-    elif (rating[i] > new_el or rating[i] == new_el) and rating[i + 1] < new_el:
-        rating.insert(i + 1, new_el)
-        break
-print(rating)
+# Реализовать формирование списка, используя функцию range() и возможности генератора.
+# В список должны войти четные числа от 100 до 1000 (включая границы).
+# Необходимо получить результат вычисления произведения всех элементов списка.
+# Подсказка: использовать функцию reduce().
+from functools import reduce
 
+def multiplication(a, b):
+    return a * b
+
+my_list = [i for i in range(100, 1001) if i % 2 == 0]
+
+print(reduce(multiplication,my_list))
